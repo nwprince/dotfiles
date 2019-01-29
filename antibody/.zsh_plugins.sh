6a56ae4842e9c4c0467ffbb642d1b7d599961f6d
nwprince <nwprince@gmail.com>
2019-01-28 21:31:03 -0600

    initial commit

---
+++ antibody/.zsh_plugins.sh
@@ -0,0 +1,12 @@
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-desyncr-SLASH-auto-ls/auto-ls.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-desyncr-SLASH-auto-ls )
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /home/nwprince/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
