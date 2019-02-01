946e42704188073c081ab7dfdc2be0ca3cc7adb0
nwprince <nwprince@gmail.com>
2019-01-31 19:43:04 -0600

    added pacman mirrorupgrade hook to speed up downloads

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 ln -sv "~/.dotfiles/.zshrc" ~
 ln -sv "~/.dotfiles/docker" ~/docker
+
+sudo mkdir ~/etc/pacman.d/hooks
+sudo mv "~/.dotfiles/pacman.d/mirrorupgrade.hook" ~/etc/pacman.d/hooks/
+sudo pacman -Syyuu
