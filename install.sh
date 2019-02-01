36fee3ab27e0272d3ffb910d6fefc69fb94c6a0f
nwprince <nwprince@gmail.com>
2019-01-31 21:10:12 -0600

    upgrade install script

--- install.sh
+++ install.sh
@@ -2,7 +2,24 @@
 
 ln -sv "~/.dotfiles/.zshrc" ~
 ln -sv "~/.dotfiles/docker" ~/docker
-
+mkdir ~/.ssh/config
+ln -sv "~/.dotfiles/.ssh/config" ~/.ssh/confing
 sudo mkdir ~/etc/pacman.d/hooks
 sudo mv "~/.dotfiles/pacman.d/mirrorupgrade.hook" ~/etc/pacman.d/hooks/
 sudo pacman -Syyuu
+sudo pacman -S yay
+
+#Setup ssh
+ssh-keygen -t rsa -b 4096 -C "nwprince@gmail.com"
+setupSSH
+echo 'Paste key in Github Settings -> SSH and GPG keys -> New SSH key\n'
+echo 'Github.com: https://github.com/settings/keys\n\n'
+echo "SSH key: "
+cat ~/.ssh/id_rsa
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  yay -S --noconfirm --noanswer-edit --save $p
+done < peptides.txt
+
+ln -sv "~/.dotfiles/vscode-insiders/settings.json" ~/.config/Code\ -\ Insiders/User/settings.json
---
+++ readme.md
@@ -0,0 +1,5 @@
+##Install instructions
+
+<H1>Install instructions</H1>
+1. sudo ./install.sh
+2. 
